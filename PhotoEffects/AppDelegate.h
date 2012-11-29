48c8f70de7f0beb08d74a62d194b00d788d3e4bb
Vincenzo Romano <elchief84@iMac-di-Vincenzo.local>
2012-11-29 22:50:18 +0100

    empty project

---
+++ PhotoEffects/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoEffects
+//
+//  Created by Vincenzo Romano on 29/11/12.
+//  Copyright (c) 2012 Vincenzo Romano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
